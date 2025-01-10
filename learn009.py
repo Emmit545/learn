1d0b83929eeb9c6c5c1843120d8d38da7da3dd9a
Emmit545 <emmittclyded9@gmail.com>
2025-01-10 22:47:12 +0800

    Create learn009.py

---
+++ learn009.py
@@ -0,0 +1,17 @@
+"""
+判断一个数是否为质数可以使用以下方法，时间复杂度较低
+"""
+
+
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(7))  # 输出: True
+print(is_prime(10))  # 输出: False
